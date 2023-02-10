76412185af41c4f087c9d6828985098e8a5c0cd1
Maeva SANCIO <maeva.sancio@mail-esd.com>
2023-02-10 01:07:09 +0100

    01:07

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+const card = document.querySelector(".card");
+document.addEventListener("mousemove", function (e) {
+  let xAxis = (window.innerWidth / 2 - e.pageX) / 50;
+  let yAxis = (window.innerHeight / 2 - e.pageY) / 50;
+  card.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
+});
+
 const images = document.querySelectorAll(".flip");
 
 console.log(images);
